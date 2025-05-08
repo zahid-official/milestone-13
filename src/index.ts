a3bcec33b1e4764e4ba22fbe1d681121dbbac6c2
zahid-official <zahid.official8@gmail.com>
2025-05-08 16:46:17 +0600

    Problem-2 with Examples

--- src/index.ts
+++ src/index.ts
@@ -8,14 +8,19 @@ formatString("Hello");
 formatString("Hello", true);
 formatString("Hello", false);
 
-
-
 function filterByRating(
   items: { title: string; rating: number }[]
 ): { title: string; rating: number }[] {
   const filtered = items.filter((item) => item.rating > 4);
   return filtered;
 }
+
+const books = [
+  { title: "Book A", rating: 4.5 },
+  { title: "Book B", rating: 3.2 },
+  { title: "Book C", rating: 5.0 },
+];
+filterByRating(books);
 
 function concatenateArrays<T>(...arrays: T[][]): T[] {
   const arr = [] as T[];
@@ -89,5 +94,3 @@ async function squareAsync(n: number): Promise<number> {
   });
 }
 
-squareAsync(4).then(console.log);
-squareAsync(-4).catch(console.error);
